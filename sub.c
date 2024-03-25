28b135b2ef699a89c547fa91ef1a941b502285e2
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-25 04:38:07 +0000

    Added new

---
+++ sub.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  int d = 0;
+  int e = 0;
+  int f = 0;
+  int g = 0;
+  int h = 0;
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  int l = 0;
+  int m = 0;
+  int n = 0;
+}
